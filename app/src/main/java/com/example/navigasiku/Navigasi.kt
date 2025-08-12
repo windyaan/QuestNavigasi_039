86d3d88a76bea51b09dd0d8580c0ce31b41d09fd
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-12 16:11:08 +0700

    fun DataApp

--- app/src/main/java/com/example/navigasiku/Navigasi.kt
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -16,3 +16,30 @@ enum class Navigasi {
     Detail
 }
 
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+
+            modifier = Modifier.padding(isiRuang)){
+            composable(route = Navigasi.Formulir.name){
+                FormIsian (
+                    //pilihanJK = JenisK.map{ id -> konteks.resources.getString(id) },
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name){
+                TampilData(
+                    onBackBtnClick = {cancelAndBackToFormulir(navController)}
+                )
+            }
+        }
+    }
+}
+
