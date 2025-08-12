4c5f3a1bcddfd315a0e60972d40feba475293c30
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-12 16:08:27 +0700

    enum class

--- app/src/main/java/com/example/navigasiku/Navigasi.kt
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -1,2 +1,18 @@
 package com.example.navigasiku
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.navigasiku.view.FormIsian
+import com.example.navigasiku.view.TampilData
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
